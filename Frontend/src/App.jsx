66b72b758faeb069df8fed1b63cf8cff7f0a8021
swaraj sahil <swarajsahil4646@gmail.com>
2025-06-30 15:22:54 +0530

    Fix admin part

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import { HashRouter as Router, Route, Routes } from 'react-router-dom'
 import Navbar from './Components/Navbar';
 import Hero from './Components/Hero';
 import './App.css';
