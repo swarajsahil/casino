19a2711d4d16144d13d59543fca89d92f12a3875
swaraj sahil <swarajsahil4646@gmail.com>
2025-06-30 14:09:41 +0530

    add

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -9,8 +9,8 @@ export default defineConfig({
   resolve: {
     alias: {
     "@": path.resolve(__dirname, "./src"),
-    Components: path.resolve(__dirname, ".src/com"),
-    ui: path.resolve(__dirname, "./src/Components/ui"),
+    components: path.resolve(__dirname, ".src/components"),
+    ui: path.resolve(__dirname, "./src/components/ui"),
     lib: path.resolve(__dirname, "./src/lib"),
     utils: path.resolve(__dirname, "./src/lib/utils"),
     hooks: path.resolve(__dirname, "./src/hooks"),
