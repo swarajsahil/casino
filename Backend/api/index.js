e042d8eb783f7c089fb682998ba32688968ee2fd
swaraj sahil <swarajsahil4646@gmail.com>
2025-08-20 21:57:01 +0530

    Add changes

--- Backend/api/index.js
+++ Backend/api/index.js
@@ -1,4 +1,8 @@
 import app from "../src/app.js";
 import serverless from "serverless-http";
+import connectDB from "../src/data/db.js";
+
+// Connect to DB (make sure env vars are set in Vercel dashboard)
+connectDB();
 
 export const handler = serverless(app);
