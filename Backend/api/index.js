6167abde76b29b72e7912f60e127aae9d1749fc0
swaraj sahil <swarajsahil4646@gmail.com>
2025-08-20 22:20:44 +0530

    export problem

--- Backend/api/index.js
+++ Backend/api/index.js
@@ -1,8 +1,10 @@
 import app from "../src/app.js";
-import serverless from "serverless-http";
+// import serverless from "serverless-http";
 import connectDB from "../src/data/db.js";
 
 // Connect to DB (make sure env vars are set in Vercel dashboard)
 connectDB();
 
-export const handler = serverless(app);
+export default function handler(req, res) {
+  return app(req, res);
+}
